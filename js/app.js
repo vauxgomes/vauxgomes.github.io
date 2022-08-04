6db8b3790c767df1683b2803eb4edee7f9d90ba6
vaux.gomes <vauxsandino@gmail.com>
2022-08-04 11:27:35 -0300

    Schedule page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* Topbar navigation */
+
 document.querySelector('#topbar-toggler').addEventListener('click', () => {
   document.querySelector('#topbar nav').classList.toggle('show')
 })
@@ -8,6 +10,18 @@ document.querySelectorAll('#topbar nav a').forEach((a) =>
   })
 )
 
+// Scroll
+window.onscroll = function () {
+  var e = document.getElementById('topbar')
+
+  if (window.pageYOffset > 10) {
+    e.classList.add('shadow')
+  } else {
+    e.classList.remove('shadow')
+  }
+}
+
+/* Experiences */
 document.querySelectorAll('.experience').forEach((item) => {
   item.addEventListener('click', () => {
     target = item.getAttribute('data-target')
@@ -20,13 +34,37 @@ document.querySelectorAll('.experience').forEach((item) => {
   })
 })
 
-// Nav scroll
-window.onscroll = function () {
-  var e = document.getElementById('topbar')
+/* Calendar */
+const showActivities = (dayIndex) => {
+  days[dayIndex].classList.add('active')
 
-  if (window.pageYOffset > 10) {
-    e.classList.add('shadow')
-  } else {
-    e.classList.remove('shadow')
-  }
+  activities.innerHTML = ''
+  schedule[dayIndex].activities.forEach((act) => {
+    const tr = document.createElement('tr')
+
+    let td = document.createElement('td')
+    td.innerHTML = `${act.start} - ${act.end}`
+    tr.append(td)
+
+    td = document.createElement('td')
+    td.innerHTML = act.activity
+    tr.append(td)
+
+    activities.append(tr)
+  })
 }
+
+const activities = document.querySelector('#activities tbody')
+const days = document.querySelectorAll('.day-name')
+const today = new Date()
+
+if (today.getDay() <= 4 && today.getDay() > 1) {
+  showActivities(today.getDay() - 2)
+}
+
+days.forEach((day, index) => {
+  day.addEventListener('click', (e) => {
+    document.querySelector('.day-name.active').classList.remove('active')
+    showActivities(index)
+  })
+})
---
+++ js/data/schedule.js
@@ -0,0 +1,102 @@
+const schedule = [
+  {
+    day: 'Monday',
+    activities: [
+      {
+        start: '07:30',
+        end: '11:30',
+        activity: 'Coordenação RDC'
+      },
+      {
+        start: '13:30',
+        end: '16:30',
+        activity: 'Reuniões'
+      }
+    ]
+  },
+  {
+    day: 'Tuesday',
+    activities: [
+      {
+        start: '07:30',
+        end: '11:30',
+        activity: 'Coordenação RDC'
+      },
+      {
+        start: '13:30',
+        end: '16:30',
+        activity: 'Coordenação RDC'
+      }
+    ]
+  },
+  {
+    day: 'Wednesday',
+    activities: [
+      {
+        start: '07:30',
+        end: '08:59',
+        activity: 'Coordenação RDC'
+      },
+      {
+        start: '09:00',
+        end: '11:00',
+        activity: 'Aula'
+      },
+      {
+        start: '13:00',
+        end: '15:00',
+        activity: 'Atendimento a aluno'
+      }
+    ]
+  },
+  {
+    day: 'Thursday',
+    activities: [
+      {
+        start: '07:00',
+        end: '08:59',
+        activity: 'Aula'
+      },
+      {
+        start: '09:20',
+        end: '11:30',
+        activity: 'Aula'
+      },
+      {
+        start: '13:30',
+        end: '14:20',
+        activity: 'Aula'
+      },
+      {
+        start: '15:00',
+        end: '17:00',
+        activity: 'Aula'
+      }
+    ]
+  },
+  {
+    day: 'Friday',
+    activities: [
+      {
+        start: '07:30',
+        end: '08:20',
+        activity: 'Aula'
+      },
+      {
+        start: '08:21',
+        end: '09:19',
+        activity: 'Cafezinho'
+      },
+      {
+        start: '09:20',
+        end: '11:30',
+        activity: 'Aula'
+      },
+      {
+        start: '13:00',
+        end: '17:00',
+        activity: 'Reuniões/Preparação de aula'
+      }
+    ]
+  }
+]
